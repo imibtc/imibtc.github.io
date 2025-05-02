e7eff1ffe77205ba40f2c035ed1458445107bb18
imibtc <113363320+imibtc@users.noreply.github.com>
2025-05-03 01:11:03 +0800

    更新 config.js

--- tv/js/config.js
+++ tv/js/config.js
@@ -1,6 +1,10 @@
 // 全局常量配置
 
-const PROXY_URL = 'https://cors.zme.ink/';
+const PROXY_URLS = [
+    'https://cors.zme.ink/',
+    'https://cors-anywhere.herokuapp.com/',
+    'https://api.codetabs.com/v1/proxy/?quest='
+];
 const HOPLAYER_URL = 'https://hoplayer.com/index.html';
 const SEARCH_HISTORY_KEY = 'videoSearchHistory';
 const MAX_HISTORY_ITEMS = 5;
